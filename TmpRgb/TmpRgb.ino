40f111eb2a41826aeb6f3ba0fcb31ebb718d53f3
epellis <nedellis1234@gmail.com>
2015-11-17 09:09:42 -0500

    First functional code
    Now changes color based on temperature accurately

--- TmpRgb/TmpRgb.ino
+++ TmpRgb/TmpRgb.ino
@@ -1,12 +1,12 @@
 int sensorPin = 0;
-int interval = 1000;
+int interval = 400;
+
+int redPin = 9;
+int bluePin = 11;
+int greenPin = 10;
 
 void setup() {
   Serial.begin(9600);
-
-  int redPin = 9;
-  int bluePin = 10;
-  int greenPin = 11;
   
   pinMode(redPin, OUTPUT);
   pinMode(greenPin, OUTPUT);
@@ -15,18 +15,21 @@ void setup() {
 
 void loop() {
   int reading = analogRead(sensorPin);
-  int intensity = (reading - 140) * 16;
+  int intensity = (reading - 140) * 18;
+
+  Serial.println(intensity);
 
   int blue = 255 - intensity;
   int red = intensity;
 
+  float voltage = (reading * 5.0)/1024;
   float temperatureC = (voltage - 0.5) * 100;
 
   analogWrite(redPin, red);
   analogWrite(bluePin, blue);
 
   Serial.print(temperatureC);
-  Serial.println(" degrees F");
+  Serial.println(" degrees C");
 
   delay(interval);
 }
