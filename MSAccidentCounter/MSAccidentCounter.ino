f07325e3f64452e717d5c0b7dfecf3d5e7536ab3
Ned <nedellis1234@gmail.com>
2016-06-07 14:58:19 -0400

    Added timer and button reset functionality

---
+++ MSAccidentCounter/MSAccidentCounter.ino
@@ -0,0 +1,83 @@
+#include <TimeLib.h>
+#include <LiquidCrystal.h>
+
+#define TIME_HEADER  "T"   // Header tag for serial time sync message
+#define TIME_REQUEST  7    // ASCII bell character requests a time sync message 
+
+/* Initialize pins for LCD */
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+const int buttonPin = 10;
+int buttonState = 0;
+
+void setup() {
+  /* Initialize LCD */
+  lcd.begin(16, 2);
+
+  /* Initialize button */
+  pinMode(buttonPin, INPUT);
+
+  /* Initialize timer */
+  setTime(0);
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  /* Clear the LCD */
+  lcd.clear();
+  
+  /* Check to see if the button has been pressed */
+  buttonState = digitalRead(buttonPin);
+  if (buttonState == HIGH) {
+      setTime(0); // Reset the timer to zero
+  }
+
+  /* Call the print time method to determine the time and print it */
+  printTime();
+
+  Serial.println(now());
+
+  delay(1000);
+  
+}
+
+String isPlural(int num) {
+  /* Returns an s if the time unit is plural (e.g. more than 1) */
+  if (num != 1) {
+    return "s";
+  }
+  else return "";
+}
+
+void preAmble() {
+  lcd.begin(16, 2);
+  lcd.print("Accident free");
+  lcd.setCursor(0, 1);
+  lcd.print("for ");
+}
+
+void printTime() {
+
+  /* Print the preamble */
+  preAmble();
+  
+  /* Find out which time unit is most appropriate */
+  if (month() > 1) {
+    lcd.print(month());
+    lcd.print(" month" + isPlural(month()));
+  } else if (day() > 1) {
+    lcd.print(day());
+    lcd.print(" day" + isPlural(day()));
+  } else if (hour() > 1) {
+    lcd.print(hour());
+    lcd.print(" hour" + isPlural(hour()));
+  } else if (minute() > 1) {
+    lcd.print(minute());
+    lcd.print(" minute" + isPlural(minute()));
+  } else {
+    lcd.print(second());
+    lcd.print(" second" + isPlural(second()));
+  }
+}
+
