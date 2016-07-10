bf996ebe3b9211141771302318c806e82dc0fac6
Ned <nedellis1234@gmail.com>
2016-07-10 11:01:12 -0500

    Added demo to convert and store ints using bitwise function

---
+++ MSAccidentCounter/SaveNumsDemo/SaveNumsDemo.ino
@@ -0,0 +1,67 @@
+#include <EEPROM.h>
+
+/** Generate a set of 10 integers and then store them in EEPROM for retrival */
+void setup() {
+  
+  Serial.begin(9600);
+  int address = 0;
+  int nums [10];
+
+  for (int i = 0; i < 10; i++) {
+    nums[i] = rand();
+  }
+
+  for (int i = 0; i < 10; i++) {
+    
+    Serial.print(nums[i]);
+    Serial.print("\n");
+    
+  }
+
+  Serial.println();
+
+  for (int i = 0; i < 10; i++) {
+        
+    unsigned char bytes[4];
+    unsigned long num = nums[i];
+
+    bytes[0] = (num >> 8) & 0xFF;
+    bytes[1] = num & 0xFF;
+
+    EEPROM.write(address, bytes[0]);
+    address++;
+    EEPROM.write(address, bytes[1]);
+    address++;
+
+    Serial.print(nums[i]);
+    Serial.print(":\t\t");
+    Serial.print((unsigned char)bytes[0]);
+    Serial.print("\t");
+    Serial.print((unsigned char)bytes[1]);
+    Serial.println();
+    
+  }
+
+  Serial.println();
+
+  for (int i = 0; i < 10; i++) {
+    int readAddress = 2 * i;
+
+    Serial.print((unsigned char)EEPROM.read(readAddress));
+    Serial.print("\t");
+    Serial.print((unsigned char)EEPROM.read(readAddress + 1));
+    Serial.println();
+
+    int num;
+
+    num = EEPROM.read(readAddress) * 256;
+    num += EEPROM.read(readAddress + 1);
+
+    Serial.print(num);
+    Serial.println();
+  }
+
+}
+
+void loop() {
+}
--- rgbMATRIX/rgbMATRIX.ino
+++ rgbMATRIX/rgbMATRIX.ino
@@ -1,5 +1,9 @@
 #include <Colorduino.h>
 
+ int b = 0;
+ int g = 255;
+ int polarity = 1;
+
 void setup() {
   Colorduino.Init();
 
@@ -9,5 +13,14 @@ void setup() {
 }
 
 void loop() {
-  Colorduino.ColorFill(0, 255, 255);
+  if (b < 0 || b > 255) {
+    polarity = polarity * -1;
+  }
+
+  b += polarity;
+  g = 255 - b;
+
+  Colorduino.ColorFill(0, g, b);
+  
+  delay(100);
 }
